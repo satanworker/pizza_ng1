f240cd376a4ffca7cefa254f14d6a42648328fa9
satanworker <satanworker@gmail.com>
2016-08-19 01:26:46 +0300

    some fixes on UI

--- src/app/components/createPizza/createPizza.controller.js
+++ src/app/components/createPizza/createPizza.controller.js
@@ -9,6 +9,8 @@ class CreatePizzaController {
     };
     this.inputData = JSON.parse(this.comment);
     this.toppings = this.inputData.toppings;
+    this.unfilled = false;
+    this.error = '';
     this.pizzas = [];
   }
   choseTopping(topping, toppingID){
@@ -17,49 +19,48 @@ class CreatePizzaController {
       item['chosen'] = false;
     })
     topping['chosen'] = true;
-    console.log(this.pizza)
   }
   removePizza($event, id) {
-    console.log('removePizza', id);
     $event.preventDefault();
     this.pizzas.splice(id, 1);
   }
+  showError(error) {
+    this.unfilled = true;
+    this.error = error;
+    setTimeout(() => {
+      this.unfilled = false;
+      this.error = '';
+    }, 1000);
+  }
   submit($event){
     $event.preventDefault();
-    let unfilled = false;
-    let error = '';
+    this.error = '';
     if(this.pizza.size) {
-      if(this.pizza.size < 10) {
-        error = 'Too small!';
-        unfilled = true;
+      if (this.pizza.size < 10) {
+        this.showError('Too small!');
       }
-      if(this.pizza.size > 100)
-      {
-        error = 'Too BIG!';
-        unfilled = true;
+      if (this.pizza.size > 100) {
+        this.showError('Too BIG!');
       }
-    } else unfilled = true;
-    if(!this.pizza.name) {
-      unfilled = true;
-      error = "There aint no unnamed pizza in this world"
+    } else this.showError('Some size, please');
+    if (!this.pizza.name) {
+      this.showError('Pizza must be named!');
     }
-    if(!this.pizza.topping) {
-      unfilled = true;
-      error = "Wait, no topping for pizza?!";
+    if (!this.pizza.topping) {
+      this.showError('Wait, no topping for pizza?!!');
+
     }
-    if (unfilled) {
-      alert(error);
-    } else {
-      console.log('Submit Pizza', this.pizza);
-      alert('Submitted your pizza!');
+    if (!this.unfilled) {
       this.pizzas.push(this.pizza);
       this.pizza = {
         size: null,
         name: null,
         topping: null
       };
+      this.toppings.map(item => {
+        item['chosen'] = false;
+      })
     }
-
 
   }
 }
